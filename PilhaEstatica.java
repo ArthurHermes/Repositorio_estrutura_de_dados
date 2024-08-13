f46115fcf529c6c34586d87092577439cd4d4441
ArthurHermes <arthurfhermes@gmail.com>
2024-08-13 15:39:42 -0300

    Finish Pilha estica

--- PilhaEstatica.java
+++ PilhaEstatica.java
@@ -1,16 +1,44 @@
-public class PilhaEstatica{
+public class PilhaEstatica {
     private int[] numero;
     private int topo;
-    
-    public PilhaEstatica(int tamanho){
+
+    public PilhaEstatica(int tamanho) {
         numero = new int[tamanho];
-        topo = - 1;
+        topo = -1;
     }
 
-    public void empilhar(int item){
-        if (topo == numero.length -1){
-            throw new RuntimeException("Pilha esta cheia");
+    public void empilhar(int item) {
+        if (topo == numero.length - 1) {
+            throw new RuntimeException("Pilha está cheia");
         }
-        numero[++topo] = item
+        numero[++topo] = item;
     }
+
+    public int desempilhar() {
+        if (isEmpty()) {
+            throw new RuntimeException("Pilha está vazia");
+        }
+        return numero[topo--];
+    }
+
+    public boolean isEmpty() {
+        return topo == -1;
+    }
+
+    public boolean isFull() {
+        return topo == numero.length - 1;
+    }
+
+
+    public static void main(String[] args) {
+        PilhaEstatica p = new PilhaEstatica(5);
+
+        p.empilhar(2);
+
+        System.out.println(p.isEmpty());
+
+        System.out.println(p.isFull());
+
+    }
+
 }
