2a3691b7a9d366c59f3035a5f06e43720cc2914c
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-08-27 10:33:27 -0300

    c

---
+++ Lista/StaticList.java
@@ -0,0 +1,36 @@
+package Lista;
+
+public class StaticList{
+    private int data;
+
+
+
+
+    public StaticList(){
+
+    }
+
+    public void clear(){
+
+    }
+    public void add(int data){
+
+    }
+    public void add(int data, int pos){
+
+    }
+    public int remove(int data){
+        return 0;
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+    public boolean isFull(){
+        return true;
+    }
+
+    public void setData(int pos, int data){
+
+    }
+}
