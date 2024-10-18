a25ae8f3d89d3386433ee8411d3206103f54e82a
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-10-18 08:27:03 -0300

    n

--- TabelaHash/HashTable.java
+++ TabelaHash/HashTable.java
@@ -1,26 +1,30 @@
 package TabelaHash;
 
-
 public class HashTable {
     private Integer[] table;
     private int size;
 
     public HashTable(int size) {
+    
         this.size = size;
         this.table = new Integer[size];
     }
 
 
     private int hashFunction(int key) {
-        double A = (Math.sqrt(5) - 1) / 2; // // Notação de Knuth --> link(https://pt.wikipedia.org/wiki/Nota%C3%A7%C3%A3o_de_Knuth)
+        double A = (Math.sqrt(5) - 1) / 2; // Notação de Knuth --> link(https://pt.wikipedia.org/wiki/Nota%C3%A7%C3%A3o_de_Knuth)
         return (int) (size * ((key * A) % 1));
     }
 
     public void insert(int key) { // função de inserção
+
         int index = hashFunction(key);
         int originalIndex = index;
+
         while (table[index] != null && table[index] != 0) {
+
             index = (index + 1) % size;
+
             if (index == originalIndex) {
                 System.out.println("Tabela cheia! Não é possível inserir a chave " + key);
                 return;
@@ -31,13 +35,18 @@ public class HashTable {
     }
 
     public boolean search(int key) { // função de busca
+
         int index = hashFunction(key);
         int originalIndex = index;
+
         while (table[index] != null) {
+
             if (table[index] == key) {
                 return true;
             }
+
             index = (index + 1) % size;
+
             if (index == originalIndex) {
                 break;
             }
@@ -46,15 +55,20 @@ public class HashTable {
     }
 
     public void remove(int key) { // remoção de valor da TabelaHash
+
         int index = hashFunction(key);
         int originalIndex = index;
+
         while (table[index] != null) {
+
             if (table[index] == key) {
                 table[index] = 0;
                 System.out.println("Chave " + key + " removida da posição " + index + ".");
                 return;
             }
+
             index = (index + 1) % size;
+
             if (index == originalIndex) {
                 break;
             }
